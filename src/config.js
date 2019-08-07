347eb2623ae62ebf9e0c74113d00eeba8ef142d8
LukeHxH <lucasmed812@gmail.com>
2019-08-07 17:37:50 -0300

    Adiciona Babel e refatora server e config

---
+++ src/config.js
@@ -0,0 +1,15 @@
+const config = {};
+
+let port = (process.env.NODE_ENV === 'production') ? process.env.PORT : null;
+
+config.mongoDB = {
+  mongoDBUri: `${ process.env.MONGODB_URI }`,
+}
+
+config.host = {
+  baseUrl: process.env.SERVER_BASE_URL || 'http://localhost',
+  port: port || process.env.SERVER_PORT || 3333,
+  production: process.env.NODE_ENV === 'production',
+}
+
+export default config;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
-const cors = require('cors');
-const MongoAuth = require('./MongoAuth');
-/* Você deve criar um aquivo MongoAuth.js e colocar no module.exports o 
-mongoString com a string de autenticação do seu banco de dados do Mongo Atlas */
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import routes from './routes';
+import cors from 'cors';
+import config from './config';
+
+const { port } = config.host;
+const { mongoDBUri } = config.mongoDB;
 
 const server = express(); // Inicia um servidor express
 
-mongoose.connect(MongoAuth.mongoString, { useNewUrlParser: true });
+mongoose.connect(mongoDBUri, { useNewUrlParser: true });
 // Faz conexão com o banco de dados
 
 server.use(cors());
 server.use(express.json()); // Diz que o servidor precisa reconhecer JSON.
-server.use(routes); // Aplica as rotas definidas em routes.js
+server.use(routes); // Aplica as rotas definidas em routes.js.
 
-server.listen(3333); // Roda o servidor em https://localhost:3333/
+server.listen({ port }, () => {
+  console.log(`Server listening on port ${port}`);
+}); // Roda o servidor no endereço especificado.
